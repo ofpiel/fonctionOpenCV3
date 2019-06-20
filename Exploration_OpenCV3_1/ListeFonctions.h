325ad66555ee4d5873ab8a03cb66539b7eb3b307
Ofpiel FOX <ofpiel.fox@gmail.com>
2019-06-20 14:32:13 +0200

    ajout des fichier du projet
    Tous les fichiers du projet Exploration_OpenCV3_1 ont été ajoutés

---
+++ Exploration_OpenCV3_1/ListeFonctions.h
@@ -0,0 +1,13 @@
+#pragma once
+class ListeFonctions
+{
+public:
+	ListeFonctions();
+	~ListeFonctions();
+
+	static void lecteurImage();
+	static void lecteurVideo();
+	static void captureImageVideo();
+	static void lecturePlusieursFlux();
+};
+
